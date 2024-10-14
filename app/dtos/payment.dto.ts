d71e5b2a6709830ab4ca04fc9bc4c8e6335700e4
indiodev <jhollyfer.fr@gmail.com>
2024-10-13 21:49:53 -0300

    changed setup

--- app/dtos/payment.dto.ts
+++ app/dtos/payment.dto.ts
@@ -3,5 +3,4 @@ import Responsible from '#models/responsible.model'
 import { PaymentUpdateSchema } from '#validators/payment.validator'
 import { Infer } from '@vinejs/vine/types'
 
-export type ResponsiblePaymentPaginate = BasePaginate & { responsible?: Responsible | null }
 export type PaymentUpdate = Infer<typeof PaymentUpdateSchema>
